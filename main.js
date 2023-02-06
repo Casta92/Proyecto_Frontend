2d32eba53950e6c0bdfee65cb941b68e9946e88a
Casta92 <afcastanedam@gmail.com>
2023-02-06 10:38:53 -0500

    Despliegue de menús con css y JS

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const detalleProductosCarrito = document.querySelector('.product-detail');
 
-const menuHamIcon = document.querySelector('.menu')
-const mobileMenu = document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-menuHamIcon.addEventListener('click', toggleMobileMenu)
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleDetalleCarrito);
+
 
 function toggleDesktopMenu(){
+    const isCarritoClosed = detalleProductosCarrito.classList.contains('inactive');
+
+    if (!isCarritoClosed) {
+        detalleProductosCarrito.classList.add('inactive')            
+    }  
     // console.log("diste click")
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu (){
-    // console.log("diste click")
+    const isCarritoClosed = detalleProductosCarrito.classList.contains('inactive');
+
+    if (!isCarritoClosed) {
+        detalleProductosCarrito.classList.add('inactive')            
+    } 
     mobileMenu.classList.toggle('inactive')
+}
+function toggleDetalleCarrito (){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive')            
+    }    
+    detalleProductosCarrito.classList.toggle('inactive')
 }
